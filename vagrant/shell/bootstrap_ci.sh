29447eea30a71575b9fbc6031719697fcd2d32ba
Jozef Slezacek <jslezacek@gmail.com>
2017-04-09 03:08:24 +0200

    Added 2 VMs system and framework

--- vagrant/shell/bootstrap_ci.sh
+++ vagrant/shell/bootstrap_ci.sh
@@ -1,11 +1,16 @@
 # install jenkins
+JENKINS_JAR="/var/cache/jenkins/war/WEB-INF/jenkins-cli.jar"
+
 
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update 
 sudo apt-get -y install jenkins
+sudo service jenkins start
+sudo chkconfig jenkins on
+
 echo "Initial admin pwd: "
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
 
 echo "Installing jenkins plugins"
-# java -jar jenkins-cli.jar -s http://127.0.0.1:8080/ install-plugin <name>
+sudo java -jar $JENKINS_JAR -s http://127.0.0.1:8082/ install-plugin git
---
+++ vagrant/shell/bootstrap_framework.sh
@@ -0,0 +1,3 @@
+sudo apt-get update 
+sudo apt-get -y install jopenjdk-7-jr scala maven
+echo "10.10.10.10 sys" >> /etc/hosts
